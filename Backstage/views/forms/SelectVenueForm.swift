bf86a3dfa72e3384c798a3d00d1091a7abd5e33c
Felix <felix.tesche@gmail.com>
2020-12-06 23:56:35 +0100

    Added NavigationView Pop

--- Backstage/views/forms/SelectVenueForm.swift
+++ Backstage/views/forms/SelectVenueForm.swift
@@ -7,17 +7,23 @@ struct SelectionItemView: View {
     @Binding var selection: String
     @State var actionText = "Neuen Veranstalter hinzufügen"
     
-    @State var options = ["Test 1", "Test 2", "Test 3"]
+    @State var options = ["Freddys Rummelbude", "Zirkus Frankenstein", "Marius Olle-GoKartBahn"]
+    
+    // Used for popping Navigation State
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
 
     var body: some View{
         VStack {
             Form {
                 Section(footer: Text("Here is a detailed description of the setting.")) {
                     ForEach(0 ..< options.count, id: \.self) { index in
-
                         HStack{
                             Button(action: {
+                                // Trigger Controller
                                 self.selection  = options[index]
+                                
+                                // Pop Navigation State
+                                self.mode.wrappedValue.dismiss()
                             }){
                                 Text(options[index])
                             }
