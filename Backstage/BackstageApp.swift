d3dbe75279f9f7a55899f5cb0fcf863debc87383
Felix <felix.tesche@gmail.com>
2020-12-06 18:17:55 +0100

    Initial Commit

---
+++ Backstage/BackstageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackstageApp.swift
+//  Backstage
+//
+//  Created by Felix Tesche on 06.12.20.
+//
+
+import SwiftUI
+
+@main
+struct BackstageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Backstage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Backstage
+//
+//  Created by Felix Tesche on 06.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
